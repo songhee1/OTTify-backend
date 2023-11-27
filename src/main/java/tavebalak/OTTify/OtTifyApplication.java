105daeb1c4745aaceb634e0ef162bfe42c55284f
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-28 00:39:52 +0900

    Setting: 프로젝트 생성

---
+++ src/main/java/tavebalak/OTTify/OtTifyApplication.java
@@ -0,0 +1,13 @@
+package tavebalak.OTTify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtTifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtTifyApplication.class, args);
+	}
+
+}
