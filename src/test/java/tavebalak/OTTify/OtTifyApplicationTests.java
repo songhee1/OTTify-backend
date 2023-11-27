1faab738afa6a95b4a85ad44e0557f4e64be5655
jyjyjy25 <jyjyjy7418@gmail.com>
2023-11-28 00:39:52 +0900

    Setting: 프로젝트 생성

---
+++ src/test/java/tavebalak/OTTify/OtTifyApplicationTests.java
@@ -0,0 +1,13 @@
+package tavebalak.OTTify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtTifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
