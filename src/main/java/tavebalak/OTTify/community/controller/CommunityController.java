9c907d0fb2ddb35aceaeddb74271896d5dd281be
songhee1 <ghenrhkwk88@naver.com>
2023-12-07 16:21:19 +0900

    Feat : 토론주제 수정 API 생성
    에디터 DTO와 에딧DTO 사용

--- src/main/java/tavebalak/OTTify/community/controller/CommunityController.java
+++ src/main/java/tavebalak/OTTify/community/controller/CommunityController.java
@@ -2,13 +2,12 @@ package tavebalak.OTTify.community.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tavebalak.OTTify.common.ApiResponse;
 import tavebalak.OTTify.community.dto.CommunitySubjectCreateDTO;
+import tavebalak.OTTify.community.dto.CommunitySubjectEditDTO;
 import tavebalak.OTTify.community.service.CommunityService;
+import tavebalak.OTTify.exception.NotFoundException;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,5 +19,11 @@ public class CommunityController {
     public ApiResponse registerSubject(@RequestBody CommunitySubjectCreateDTO c){
         communityService.saveSubject(c);
         return ApiResponse.success("성공적으로 토론주제를 생성하였습니다.");
+    }
+
+    @PutMapping("/subject")
+    public ApiResponse modifySubject(@RequestBody CommunitySubjectEditDTO c) throws NotFoundException {
+        communityService.modifySubject(c);
+        return ApiResponse.success("성공적으로 토론주제를 수정하였습니다.");
     }
 }
--- src/main/java/tavebalak/OTTify/community/service/CommunityService.java
+++ src/main/java/tavebalak/OTTify/community/service/CommunityService.java
@@ -1,7 +1,10 @@
 package tavebalak.OTTify.community.service;
 
 import tavebalak.OTTify.community.dto.CommunitySubjectCreateDTO;
+import tavebalak.OTTify.community.dto.CommunitySubjectEditDTO;
+import tavebalak.OTTify.exception.NotFoundException;
 
 public interface CommunityService {
     public void saveSubject(CommunitySubjectCreateDTO c);
+    public void modifySubject(CommunitySubjectEditDTO c) throws NotFoundException;
 }
