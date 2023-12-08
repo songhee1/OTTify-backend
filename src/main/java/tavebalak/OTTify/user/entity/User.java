2959636ef69f9c63fadd3e55572b53a7513f6d80
songhee1 <ghenrhkwk88@naver.com>
2023-12-08 17:13:05 +0900

    Feat : 토론주제 조회 DTO를 활용한 페이징 조회 추가

--- src/main/java/tavebalak/OTTify/user/entity/User.java
+++ src/main/java/tavebalak/OTTify/user/entity/User.java
@@ -1,11 +1,14 @@
 package tavebalak.OTTify.user.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Getter
 public class User {
 
     @Id @GeneratedValue
