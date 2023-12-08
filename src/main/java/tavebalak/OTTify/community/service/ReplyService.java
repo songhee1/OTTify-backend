e5b5fbe019efde3b88a39089c23fe9b97fd35320
songhee1 <ghenrhkwk88@naver.com>
2023-12-08 18:55:50 +0900

    Feat : 토론 댓글 수정 에디터 추가(#2)

--- src/main/java/tavebalak/OTTify/community/service/ReplyService.java
+++ src/main/java/tavebalak/OTTify/community/service/ReplyService.java
@@ -1,13 +1,10 @@
 package tavebalak.OTTify.community.service;
 
 import tavebalak.OTTify.community.dto.ReplyCommentCreateDTO;
-import tavebalak.OTTify.community.dto.ReplyCommentEditDTO;
 import tavebalak.OTTify.community.dto.ReplyRecommentCreateDTO;
 
 public interface ReplyService {
     public void saveComment(ReplyCommentCreateDTO c);
 
     public void saveRecomment(ReplyRecommentCreateDTO c);
-
-    public void modifyComment(ReplyCommentEditDTO c);
 }
--- src/main/java/tavebalak/OTTify/community/service/ReplyServiceImpl.java
+++ src/main/java/tavebalak/OTTify/community/service/ReplyServiceImpl.java
@@ -3,18 +3,11 @@ package tavebalak.OTTify.community.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import tavebalak.OTTify.community.dto.CommunitySubjectEditorDTO;
 import tavebalak.OTTify.community.dto.ReplyCommentCreateDTO;
-import tavebalak.OTTify.community.dto.ReplyCommentEditDTO;
 import tavebalak.OTTify.community.dto.ReplyRecommentCreateDTO;
-import tavebalak.OTTify.community.entity.Community;
 import tavebalak.OTTify.community.entity.Reply;
 import tavebalak.OTTify.community.repository.CommunityRepository;
 import tavebalak.OTTify.community.repository.ReplyRepository;
-import tavebalak.OTTify.exception.ErrorCode;
-import tavebalak.OTTify.exception.NotFoundException;
-import tavebalak.OTTify.program.entity.Program;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -25,8 +18,7 @@ public class ReplyServiceImpl implements ReplyService{
     private final ReplyRepository replyRepository;
     @Override
     public void saveComment(ReplyCommentCreateDTO c) {
-        replyRepository.save(
-                Reply.builder()
+        replyRepository.save(Reply.builder()
                 .community(communityRepository.findById(c.getSubjectId()).get())
                 .content(c.getComment())
                 .build());
@@ -34,35 +26,12 @@ public class ReplyServiceImpl implements ReplyService{
 
     @Override
     public void saveRecomment(ReplyRecommentCreateDTO c) {
-        Reply reply = replyRepository.save(
-                Reply.builder()
+        Reply reply = replyRepository.save(Reply.builder()
                 .community(communityRepository.findById(c.getSubjectId()).get())
                 .content(c.getContent())
                 .build());
 
         Reply parentReply = replyRepository.findById(c.getCommentId()).get();
         parentReply.addReply(reply);
-    }
-
-    @Override
-    public void modifyComment(ReplyCommentEditDTO c) {
-        
-
-
-        Community community = communityRepository.findById(c.getSubjectId())
-                .orElseThrow(() -> new NotFoundException(ErrorCode.ENTITY_NOT_FOUND));
-
-        boolean present = programRepository.findById(c.getProgramId()).isPresent();
-        Program program = null;
-        if(!present){
-            program = programRepository.save(Program.builder().title(c.getProgramTitle()).posterPath(c.getPosterPath()).build());
-        }else {
-            program = programRepository.findById(c.getProgramId()).get();
-        }
-
-        CommunitySubjectEditorDTO.CommunitySubjectEditorDTOBuilder communitySubjectEditorDTOBuilder = community.toEditor();
-        CommunitySubjectEditorDTO communitySubjectEditorDTO = communitySubjectEditorDTOBuilder.title(c.getSubjectName()).content(c.getContent()).program(program).build();
-
-        community.edit(communitySubjectEditorDTO);
     }
 }
