10a3527d01c4edadfeb8eb362d2cef1e7a1d6cc8
songhee1 <ghenrhkwk88@naver.com>
2023-12-08 18:32:23 +0900

    Feat : 토론 대댓글 생성 API 추가(#1)

--- src/main/java/tavebalak/OTTify/community/service/ReplyService.java
+++ src/main/java/tavebalak/OTTify/community/service/ReplyService.java
@@ -1,7 +1,10 @@
 package tavebalak.OTTify.community.service;
 
 import tavebalak.OTTify.community.dto.ReplyCommentCreateDTO;
+import tavebalak.OTTify.community.dto.ReplyRecommentCreateDTO;
 
 public interface ReplyService {
     public void saveComment(ReplyCommentCreateDTO c);
+
+    public void saveRecomment(ReplyRecommentCreateDTO c);
 }
