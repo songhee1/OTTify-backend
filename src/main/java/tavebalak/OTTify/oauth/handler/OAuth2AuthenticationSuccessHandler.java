c7e698fe794e9edbb5c1ecab31e727f6cceb3635
siss-ankisile <ankisile@gmail.com>
2023-12-15 18:40:44 +0900

    Setting : SecurityConfig 기본 세팅 (#6)

---
+++ src/main/java/tavebalak/OTTify/oauth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -0,0 +1,8 @@
+package tavebalak.OTTify.oauth.handler;
+
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+}
---
+++ src/main/java/tavebalak/OTTify/oauth/service/CustomOAuth2UserService.java
@@ -0,0 +1,8 @@
+package tavebalak.OTTify.oauth.service;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+}
