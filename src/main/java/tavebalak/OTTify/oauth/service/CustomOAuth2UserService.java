3b2b4038d7b57d415091de084633082069e04f96
siss-ankisile <ankisile@gmail.com>
2023-12-19 01:28:25 +0900

    Feat : Oauth2UserService custom 작성 (#6) - 로그인 후 받아온 유저정보를 db에 저장

--- src/main/java/tavebalak/OTTify/oauth/service/CustomOAuth2UserService.java
+++ src/main/java/tavebalak/OTTify/oauth/service/CustomOAuth2UserService.java
@@ -1,8 +1,76 @@
 package tavebalak.OTTify.oauth.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import tavebalak.OTTify.oauth.CustomOAuth2User;
+import tavebalak.OTTify.oauth.OAuthAttributes;
+import tavebalak.OTTify.oauth.constant.SocialType;
+import tavebalak.OTTify.user.entity.User;
+import tavebalak.OTTify.user.repository.UserRepository;
 
+import java.util.Collections;
+import java.util.Map;
+
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final UserRepository userRepository;
+
+    private static final String NAVER = "naver";
+    private static final String GOOGLE = "google";
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        log.info("CustomOAuth2UserService - OAuth2 로그인 요청 진입");
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        SocialType socialType = getSocialType(registrationId);
+//        이부분 변경 고민
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName(); // OAuth2 로그인 시 키(PK)가 되는 값 (id, sub)
+        Map<String, Object> attributes = oAuth2User.getAttributes(); // 소셜 로그인에서 API가 제공하는 userInfo의 Json 값(유저 정보들)
+
+        OAuthAttributes extractAttributes = OAuthAttributes.of(socialType, userNameAttributeName, attributes);
+
+        User createdUser = getUser(extractAttributes, socialType); // getUser() 메소드로 User 객체 생성 후 반환
+
+        // DefaultOAuth2User를 구현한 CustomOAuth2User 객체를 생성해서 반환
+        return new CustomOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority(createdUser.getRole().getKey())),
+                attributes,
+                extractAttributes.getNameAttributeKey(),
+                createdUser.getRole()
+        );
+    }
+
+    private SocialType getSocialType(String registrationId) {
+        if(NAVER.equals(registrationId)) {
+            return SocialType.NAVER;
+        }
+        else {
+            return SocialType.GOOGLE;
+        }
+    }
+
+    private User getUser(OAuthAttributes attributes, SocialType socialType) {
+        User findUser = userRepository.findBySocialTypeAndSocialId(socialType, attributes.getOauth2UserInfo().getId()).orElse(null);
+
+        if(findUser == null) {
+            return saveUser(attributes, socialType);
+        }
+        return findUser;
+    }
+
+    private User saveUser(OAuthAttributes attributes, SocialType socialType) {
+        User createdUser = attributes.toEntity(socialType, attributes.getOauth2UserInfo());
+        return userRepository.save(createdUser);
+    }
 }
--- src/main/java/tavebalak/OTTify/user/entity/User.java
+++ src/main/java/tavebalak/OTTify/user/entity/User.java
@@ -1,20 +1,48 @@
 package tavebalak.OTTify.user.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tavebalak.OTTify.oauth.constant.Role;
+import tavebalak.OTTify.oauth.constant.SocialType;
+
+import javax.persistence.*;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user_table")
 public class User {
 
     @Id @GeneratedValue
     @Column(name = "user_id")
     private Long id;
+
+    private String email;
     private String nickName;
     private String profilePhoto;
     private double averageRating;
     private int level;
-    private int socialType;
+    
+//    수정한 부분
+    // socialType
+    @Enumerated
+    private SocialType socialType;
 
+    private String socialId;
+
+    @Enumerated
+    private Role role;
+
+    @Builder
+    public User(String email, String nickName, String profilePhoto, SocialType socialType, String socialId, Role role) {
+        this.email = email;
+        this.nickName = nickName;
+        this.profilePhoto = profilePhoto;
+        this.averageRating = 0;
+        this.level = 1;
+        this.socialType = socialType;
+        this.socialId = socialId;
+        this.role = role;
+    }
 }
