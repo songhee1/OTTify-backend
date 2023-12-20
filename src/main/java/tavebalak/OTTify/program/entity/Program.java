aae71d04abdfb9eeab8a8b009ddc99090a652ee2
jyjyjy25 <jyjyjy7418@gmail.com>
2023-12-20 23:04:11 +0900

    Chore: 엔티티에 @Getter, @NoArgsConstructor 추가 (#12)

--- src/main/java/tavebalak/OTTify/program/entity/Program.java
+++ src/main/java/tavebalak/OTTify/program/entity/Program.java
@@ -1,8 +1,14 @@
 package tavebalak.OTTify.program.entity;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Program {
     @Id @GeneratedValue
     @Column(name = "program_id")
