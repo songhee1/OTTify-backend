37d29d80e16ed76f17286efe76a6393ca101a5a2
siss-ankisile <ankisile@gmail.com>
2023-12-27 18:16:01 +0900

    Feat : 추가정보 입력 DTO 생성 (#17)

---
+++ src/main/java/tavebalak/OTTify/oauth/dto/SignUpInfoDto.java
@@ -0,0 +1,10 @@
+package tavebalak.OTTify.oauth.dto;
+
+import java.util.List;
+
+
+public class SignUpInfoDto {
+    private List<Integer> ottList;
+    private Integer firstGenre;
+    private List<Integer> genreList;
+}
