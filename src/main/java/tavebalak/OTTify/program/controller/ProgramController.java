7163c295cf9cf730ec9459fab782f0ae9358d726
Jinu Yi <dionisos198@naver.com>
2024-01-03 04:56:39 +0900

    Feat: 프로그램 검색 시 page 기능 (#14)

--- src/main/java/tavebalak/OTTify/program/controller/ProgramController.java
+++ src/main/java/tavebalak/OTTify/program/controller/ProgramController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import tavebalak.OTTify.common.BaseResponse;
-import tavebalak.OTTify.program.dto.searchTrending.Response.SearchResponseDtoV1;
-import tavebalak.OTTify.program.dto.searchTrending.Response.TrendingResponseDto;
+import tavebalak.OTTify.program.dto.searchTrending.Response.*;
 import tavebalak.OTTify.program.service.ProgramShowAndSaveService;
 
 @RestController
@@ -24,8 +23,20 @@ public class ProgramController {
 
     @GetMapping("/api/v1/program/search")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<SearchResponseDtoV1> getSearchProgram(@RequestParam("name") String name){
+    public BaseResponse<SearchResponseDto> getSearchProgram(@RequestParam("name") String name){
         return BaseResponse.success(programShowAndSaveService.searchByName(name));
+    }
+
+    @GetMapping("/api/v1/movie/search")
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponse<SearchMovieResponseDto> getSearchMovieProgram(@RequestParam("name") String name, @RequestParam("page") int page){
+        return BaseResponse.success(programShowAndSaveService.searchByMovieName(name,page));
+    }
+
+    @GetMapping("/api/v1/tv/search")
+    @ResponseStatus(HttpStatus.OK)
+    public BaseResponse<SearchTvResponseDto> getSearchTvProgram(@RequestParam("name") String name, @RequestParam("page") int page){
+        return BaseResponse.success(programShowAndSaveService.searchByTvName(name,page));
     }
 
 
