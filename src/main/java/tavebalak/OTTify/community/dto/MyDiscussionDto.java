c76d31a5cfb4e25c3bf130d8cd22e1a37d12b546
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-04 03:13:14 +0900

    Feat: 내 토론 조회용 DTO 추가 (#23)

---
+++ src/main/java/tavebalak/OTTify/community/dto/MyDiscussionDto.java
@@ -0,0 +1,28 @@
+package tavebalak.OTTify.community.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class MyDiscussionDto {
+    private Long discussionId;
+    private String createdDate;
+    private String title;
+    private String programTitle;
+    private String content;
+    private String img;
+    private Long likeCnt;
+    private Long replyCnt;
+
+    @Builder
+    public MyDiscussionDto(Long discussionId, String createdDate, String title, String programTitle, String content, String img, Long likeCnt, Long replyCnt) {
+        this.discussionId = discussionId;
+        this.createdDate = createdDate;
+        this.title = title;
+        this.programTitle = programTitle;
+        this.content = content;
+        this.img = img;
+        this.likeCnt = likeCnt;
+        this.replyCnt = replyCnt;
+    }
+}
