76645ee9c574849f4c0a32752bd1d6dff90d6f3e
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-04 03:09:20 +0900

    Feat: LikedReply, LikedCommunity 엔티티 추가 (#23)

---
+++ src/main/java/tavebalak/OTTify/user/entity/LikedCommunity.java
@@ -0,0 +1,32 @@
+package tavebalak.OTTify.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import tavebalak.OTTify.community.entity.Community;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class LikedCommunity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community_id")
+    private Community community;
+
+    @Builder
+    public LikedCommunity(User user, Community community) {
+        this.user = user;
+        this.community = community;
+    }
+}
