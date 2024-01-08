5539cc97c3ea24a101c12a3b9c339ff1bd6b06a6
Jinu Yi <dionisos198@naver.com>
2024-01-08 16:14:46 +0900

    Feat: 프로그램 좋아요 및 관심없어요 기능 (#36)

--- src/main/java/tavebalak/OTTify/user/repository/LikedProgramRepository.java
+++ src/main/java/tavebalak/OTTify/user/repository/LikedProgramRepository.java
@@ -1,10 +1,17 @@
 package tavebalak.OTTify.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tavebalak.OTTify.program.entity.Program;
 import tavebalak.OTTify.user.entity.LikedProgram;
+import tavebalak.OTTify.user.entity.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LikedProgramRepository extends JpaRepository<LikedProgram, Long> {
     public List<LikedProgram> findByUserId(Long userId);
+
+    Optional<LikedProgram> findByProgramAndUser(Program program, User user);
+
+    boolean existsByProgramAndUser(Program program,User user);
 }
