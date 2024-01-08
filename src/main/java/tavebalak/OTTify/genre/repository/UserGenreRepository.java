02b83d523e2aaeddd7b61e50417028ac5a4239e6
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-08 20:35:47 +0900

    Remove: 필요없는 repository 함수 제거 (#21)

--- src/main/java/tavebalak/OTTify/genre/repository/UserGenreRepository.java
+++ src/main/java/tavebalak/OTTify/genre/repository/UserGenreRepository.java
@@ -5,8 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import tavebalak.OTTify.genre.entity.UserGenre;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.data.jpa.repository.Modifying;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,8 +22,4 @@ public interface UserGenreRepository extends JpaRepository<UserGenre, Long> {
     Boolean existsByGenreIdAndUserIdAndIsFirst(@Param("genreId") Long genreId, @Param("userId") Long userId, boolean isFirst);
     UserGenre findByGenreIdAndUserIdAndIsFirst(@Param("genreId") Long genreId, @Param("userId") Long userId, boolean isFirst);
 
-    @Transactional
-    @Modifying
-    @Query("delete from UserGenre ug where ug.genre.id in :genreIds and ug.user.id =:userId")
-    void deleteAllByIdInQuery(@Param("genreIds") List<Long> genreIds, Long userId);
 }
