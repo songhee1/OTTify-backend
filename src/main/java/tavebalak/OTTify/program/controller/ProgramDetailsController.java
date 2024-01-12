6e3c44e57b49c81c74a39c946ad8ae900db93abf
Jinu Yi <dionisos198@naver.com>
2024-01-13 03:45:08 +0900

    Docs: 프로그램 상세 정보 swagger 세팅 (#41)

--- src/main/java/tavebalak/OTTify/program/controller/ProgramDetailsController.java
+++ src/main/java/tavebalak/OTTify/program/controller/ProgramDetailsController.java
@@ -1,5 +1,8 @@
 package tavebalak.OTTify.program.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,13 +15,18 @@ import tavebalak.OTTify.program.service.ProgramDetailsShowService;
 
 @RestController
 @RequiredArgsConstructor
+@Api(tags = {"프로그램에 대한 상세정보 컨트롤러"})
+
 public class ProgramDetailsController {
+
     private final ProgramDetailsShowService programDetailsShowService;
 
-
+    @ApiOperation(value = "프로그램에 대한 상세 정보 보기", notes = "프로그램에 대한 상세 정보를 받아옵니다")
+    @ApiImplicitParam(name = "programId", dataType = "long", value = "프로그램 ID", required = true, paramType = "path")
     @GetMapping("/api/v1/program/{programId}/details")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<ProgramResponseDto> getProgramDetails(@PathVariable("programId") Long programId){
+    public BaseResponse<ProgramResponseDto> getProgramDetails(
+        @PathVariable("programId") Long programId) {
         return BaseResponse.success(programDetailsShowService.showDetails(programId));
     }
 }
