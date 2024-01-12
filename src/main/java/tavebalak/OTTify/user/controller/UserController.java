333a5d506b941af1226130aef4039dc04550e2b0
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-13 02:50:32 +0900

    Style: 엔드포인트 path parameter명 수정(id -> userId) (#57)

--- src/main/java/tavebalak/OTTify/user/controller/UserController.java
+++ src/main/java/tavebalak/OTTify/user/controller/UserController.java
@@ -32,44 +32,44 @@ public class UserController {
 
     @ApiOperation(value = "프로필 조회 api", notes = "유저 프로필(닉네임, 프로필 사진)을 조회합니다.")
     @ApiImplicitParam(name = "userId", dataType = "long", value = "유저 id", required = true, paramType = "path")
-    @GetMapping("/{id}")
+    @GetMapping("/{userId}")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<UserProfileDTO> getUserProfile(@PathVariable("id") Long userId) {
+    public BaseResponse<UserProfileDTO> getUserProfile(@PathVariable("userId") Long userId) {
         return BaseResponse.success(userService.getUserProfile(userId));
     }
 
     @ApiOperation(value = "구독 중인 OTT 조회 api", notes = "유저가 구독 중인 OTT를 조회합니다.")
     @ApiImplicitParam(name = "userId", dataType = "long", value = "유저 id", required = true, paramType = "path")
-    @GetMapping("/{id}/otts")
+    @GetMapping("/{userId}/otts")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<UserOttListDTO> getUserOTT(@PathVariable("id") Long userId) {
+    public BaseResponse<UserOttListDTO> getUserOTT(@PathVariable("userId") Long userId) {
         return BaseResponse.success(userService.getUserOTT(userId));
     }
 
     @ApiOperation(value = "프로필 수정 api", notes = "유저 프로필(닉네임, 프로필 사진)을 수정합니다.")
     @ApiImplicitParam(name = "userId", dataType = "long", value = "유저 id", required = true, paramType = "path")
     @ApiResponse(code = 200, message = "성공적으로 프로필이 업데이트 되었습니다.")
-    @PatchMapping("/{id}/profile")
+    @PatchMapping("/{userId}/profile")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<Long> updateUserProfile(@PathVariable("id") Long userId, @Validated @RequestBody UserProfileUpdateDTO updateRequestDTO) {
+    public BaseResponse<Long> updateUserProfile(@PathVariable("userId") Long userId, @Validated @RequestBody UserProfileUpdateDTO updateRequestDTO) {
         return BaseResponse.success(userService.updateUserProfile(userId, updateRequestDTO));
     }
 
     @ApiOperation(value = "구독 중인 OTT 수정 api", notes = "유저가 구독 중인 OTT를 수정합니다.")
     @ApiImplicitParam(name = "userId", dataType = "long", value = "유저 id", required = true, paramType = "path")
     @ApiResponse(code = 200, message = "성공적으로 구독 중인 OTT가 업데이트 되었습니다.")
-    @PatchMapping("/{id}/otts")
+    @PatchMapping("/{userId}/otts")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<Long> updateUserOTT(@PathVariable("id") Long userId, @RequestBody List<UserOttUpdateDTO> updateRequestDTO) {
+    public BaseResponse<Long> updateUserOTT(@PathVariable("userId") Long userId, @RequestBody List<UserOttUpdateDTO> updateRequestDTO) {
         return BaseResponse.success(userService.updateUserOTT(userId, updateRequestDTO));
     }
 
     @ApiOperation(value = "1순위 취향 장르 수정 api", notes = "유저의 1순위 취향 장르를 수정합니다.")
     @ApiImplicitParam(name = "userId", dataType = "long", value = "유저 id", required = true, paramType = "path")
     @ApiResponse(code = 200, message = "성공적으로 1순위 장르가 업데이트 되었습니다.")
-    @PatchMapping("/{id}/1stGenre")
+    @PatchMapping("/{userId}/1stGenre")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse update1stLikedGenre(@PathVariable("id") Long userId, @Validated @RequestBody GenreUpdateDTO updateRequestDto) {
+    public BaseResponse update1stLikedGenre(@PathVariable("userId") Long userId, @Validated @RequestBody GenreUpdateDTO updateRequestDto) {
         userService.update1stGenre(userId, updateRequestDto);
         return BaseResponse.success("성공적으로 1순위 장르가 업데이트 되었습니다.");
     }
@@ -77,9 +77,9 @@ public class UserController {
     @ApiOperation(value = "2순위 취향 장르 수정 api", notes = "유저의 2순위 취향 장르를 수정합니다.")
     @ApiImplicitParam(name = "userId", dataType = "long", value = "유저 id", required = true, paramType = "path")
     @ApiResponse(code = 200, message = "성공적으로 2순위 장르가 업데이트 되었습니다.")
-    @PatchMapping("/{id}/2ndGenre")
+    @PatchMapping("/{userId}/2ndGenre")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse update2ndLikedGenre(@PathVariable("id") Long userId, @Validated @RequestBody GenreUpdateDTO updateRequestDTO) {
+    public BaseResponse update2ndLikedGenre(@PathVariable("userId") Long userId, @Validated @RequestBody GenreUpdateDTO updateRequestDTO) {
         userService.update2ndGenre(userId, updateRequestDTO);
         return BaseResponse.success("성공적으로 2순위 장르가 업데이트 되었습니다.");
     }
@@ -92,9 +92,9 @@ public class UserController {
             @ApiImplicitParam(name = "sort", dataType = "String", value = "정렬기준(createdAt, updatedAt)", required = false, paramType = "path"),
             @ApiImplicitParam(name = "size", dataType = "int", value = "페이지당 아이템 갯수", required = false, paramType = "path")
     })
-    @GetMapping("/{id}/reviews")
+    @GetMapping("/{userId}/reviews")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<List<MyReviewDto>> getMyReview(@PathVariable("id") Long userId, @PageableDefault(
+    public BaseResponse<List<MyReviewDto>> getMyReview(@PathVariable("userId") Long userId, @PageableDefault(
             size = 5,
             sort = "createdAt",
             direction = Sort.Direction.DESC,
@@ -108,9 +108,9 @@ public class UserController {
             @ApiImplicitParam(name = "page", dataType = "int", value = "페이지 번호(0부터 시작)", required = false, paramType = "path"),
             @ApiImplicitParam(name = "size", dataType = "int", value = "페이지당 아이템 갯수", required = false, paramType = "path")
     })
-    @GetMapping("/{id}/likedReviews")
+    @GetMapping("/{userId}/likedReviews")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<List<MyReviewDto>> getLikedReview(@PathVariable("id") Long userId, @PageableDefault(
+    public BaseResponse<List<MyReviewDto>> getLikedReview(@PathVariable("userId") Long userId, @PageableDefault(
             size = 5,
             page = 0) Pageable pageable) {
         return BaseResponse.success(userService.getLikedReview(userId, pageable));
@@ -124,9 +124,9 @@ public class UserController {
             @ApiImplicitParam(name = "sort", dataType = "String", value = "정렬기준(createdAt, updatedAt)", required = false, paramType = "path"),
             @ApiImplicitParam(name = "size", dataType = "int", value = "페이지당 아이템 갯수", required = false, paramType = "path")
     })
-    @GetMapping("/{id}/discussion/hosting")
+    @GetMapping("/{userId}/discussion/hosting")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<List<MyDiscussionDto>> getHostedDiscussion(@PathVariable("id") Long userId, @PageableDefault(
+    public BaseResponse<List<MyDiscussionDto>> getHostedDiscussion(@PathVariable("userId") Long userId, @PageableDefault(
             size = 5,
             sort = "createdAt",
             direction = Sort.Direction.DESC,
@@ -140,9 +140,9 @@ public class UserController {
             @ApiImplicitParam(name = "page", dataType = "int", value = "페이지 번호(0부터 시작)", required = false, paramType = "path"),
             @ApiImplicitParam(name = "size", dataType = "int", value = "페이지당 아이템 갯수", required = false, paramType = "path")
     })
-    @GetMapping("/{id}/discussion/participating")
+    @GetMapping("/{userId}/discussion/participating")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<List<MyDiscussionDto>> getParticipatedDiscussion(@PathVariable("id") Long userId, @PageableDefault(
+    public BaseResponse<List<MyDiscussionDto>> getParticipatedDiscussion(@PathVariable("userId") Long userId, @PageableDefault(
             size = 5,
             page = 0) Pageable pageable) {
         return BaseResponse.success(userService.getParticipatedDiscussion(userId, pageable));
