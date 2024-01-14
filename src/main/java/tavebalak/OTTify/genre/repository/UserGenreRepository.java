a5cb146342830390ace1d895b50fc3d0324eedce
Jinu Yi <dionisos198@naver.com>
2024-01-14 23:10:03 +0900

    Fix: UserGenreRepository 오류 수정 (#54)

--- src/main/java/tavebalak/OTTify/genre/repository/UserGenreRepository.java
+++ src/main/java/tavebalak/OTTify/genre/repository/UserGenreRepository.java
@@ -1,5 +1,7 @@
 package tavebalak.OTTify.genre.repository;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -7,11 +9,9 @@ import org.springframework.stereotype.Repository;
 import tavebalak.OTTify.genre.entity.UserGenre;
 import tavebalak.OTTify.user.entity.User;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface UserGenreRepository extends JpaRepository<UserGenre, Long> {
+
     Optional<UserGenre> findByUserIdAndIsFirst(Long userId, boolean isFirst);
 
     @Query("select ug from UserGenre ug join fetch ug.genre where ug.user.id =:userId and ug.isFirst = true")
@@ -22,5 +22,6 @@ public interface UserGenreRepository extends JpaRepository<UserGenre, Long> {
 
     Optional<UserGenre> findByUserAndIsFirst(User user, boolean isFirst);
 
-    Optional<UserGenre> findByGenreIdAndUserIdAndIsFirst(Long genreId, @Param("userId") Long userId, boolean isFirst);
+    Optional<UserGenre> findByGenreIdAndUserIdAndIsFirst(Long genreId, Long userId,
+        boolean isFirst);
 }
