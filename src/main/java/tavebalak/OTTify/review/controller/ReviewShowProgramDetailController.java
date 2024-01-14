dd2f2b43b95e87b75686005149dd0a0b0b1e9176
Jinu Yi <dionisos198@naver.com>
2024-01-14 15:32:53 +0900

    Docs: 리뷰 및 리뷰 태그 관련 swagger 작성 (#54)

--- src/main/java/tavebalak/OTTify/review/controller/ReviewShowProgramDetailController.java
+++ src/main/java/tavebalak/OTTify/review/controller/ReviewShowProgramDetailController.java
@@ -1,12 +1,20 @@
 package tavebalak.OTTify.review.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import tavebalak.OTTify.common.BaseResponse;
 import tavebalak.OTTify.error.ErrorCode;
 import tavebalak.OTTify.error.exception.NotFoundException;
@@ -20,58 +28,95 @@ import tavebalak.OTTify.user.repository.UserRepository;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/{programId}")
+@Api(tags = {"프로그램 상세 페이지에서 리뷰 보여주기"})
+
 public class ReviewShowProgramDetailController {
+
     private final ReviewShowProgramDetailService reviewShowProgramDetailService;
     private final UserRepository userRepository;
 
+
+    @ApiOperation(value = "프로그램 페이지의 초기 리뷰 보여주기", notes = "프로그램 페이지의 처음 4개의 리뷰를 보여줍니다")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "programId", dataType = "long", value = "현재 프로그램의 ID", required = true, paramType = "path"),
+    })
     @GetMapping("/normal/reviews/count/4")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<ReviewResponseDtoList> show4ReviewList(@PathVariable("programId") Long programId){
+    public BaseResponse<ReviewResponseDtoList> show4ReviewList(
+        @PathVariable("programId") Long programId) {
         return BaseResponse.success(reviewShowProgramDetailService.show4Review(programId));
     }
 
-
+    @ApiOperation(value = "전체 리뷰 리스트를 보여주기", notes = "전체 리뷰 리스트를 보여줍니다")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "page", dataType = "int", value = "페이지 번호(0부터 시작)", defaultValue = "0", required = false, paramType = "query"),
+        @ApiImplicitParam(name = "direction", dataType = "String", value = "내림차순과 오름차순", required = false, defaultValue = "DESC", paramType = "query"),
+        @ApiImplicitParam(name = "sort", dataType = "String", value = "정렬기준(likeCounts,createdAt)", required = false, defaultValue = "likeCounts", paramType = "query"),
+        @ApiImplicitParam(name = "size", dataType = "int", value = "페이지당 아이템 갯수", defaultValue = "10", required = false, paramType = "query")
+    })
     @GetMapping("/normal/reviews")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<Slice<ReviewProgramResponseDto>> showReviewListALL(@PathVariable("programId") Long programId,
-                                                                           @PageableDefault(size = 10,
-                                                                                   sort = "likeNumber",
-                                                                                   direction = Sort.Direction.DESC,
-                                                                                   page = 0) Pageable pageable){
-        return BaseResponse.success(reviewShowProgramDetailService.showReviewList(programId,pageable));
+    public BaseResponse<Slice<ReviewProgramResponseDto>> showReviewListALL(
+        @PathVariable("programId") Long programId,
+        @PageableDefault(size = 10,
+            sort = "likeCounts",
+            direction = Sort.Direction.DESC,
+            page = 0) Pageable pageable) {
+        return BaseResponse.success(
+            reviewShowProgramDetailService.showReviewList(programId, pageable));
     }
 
-
+    @ApiOperation(value = "프로그램 페이지의 자신의 취향에 맞는 장르 보여주기", notes = "자신의 취향에 맞는 좋아요 순  처음 4개의 리뷰를 보여줍니다")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "programId", dataType = "long", value = "현재 프로그램의 ID", required = true, paramType = "path"),
+    })
     @GetMapping("/user/specific/reviews/count/4")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<ReviewResponseDtoList> show4UserSpecificReviewList(@PathVariable("programId") Long programId){
-        User findUser= getUser();
-        return BaseResponse.success(reviewShowProgramDetailService.show4UserSpecificReviewList(findUser,programId));
+    public BaseResponse<ReviewResponseDtoList> show4UserSpecificReviewList(
+        @PathVariable("programId") Long programId) {
+        User findUser = getUser();
+        return BaseResponse.success(
+            reviewShowProgramDetailService.show4UserSpecificReviewList(findUser, programId));
     }
 
+
+    @ApiOperation(value = "사용자의 취향에 맞는 전체 장르 리스트를 보여주기", notes = "전체 장르 리스트를 보여줍니다")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "page", dataType = "int", value = "페이지 번호(0부터 시작)", defaultValue = "0", required = false, paramType = "query"),
+        @ApiImplicitParam(name = "direction", dataType = "String", value = "내림차순과 오름차순", required = false, defaultValue = "DESC", paramType = "query"),
+        @ApiImplicitParam(name = "sort", dataType = "String", value = "정렬기준(likeCounts,createdAt)", required = false, defaultValue = "likeCounts", paramType = "query"),
+        @ApiImplicitParam(name = "size", dataType = "int", value = "페이지당 아이템 갯수", defaultValue = "10", required = false, paramType = "query")
+    })
     @GetMapping("/user/specific/reviews")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<Slice<ReviewProgramResponseDto>> showUserSpecificReviewListALL(@PathVariable("programId") Long programId,
-                                                                                       @PageableDefault(size = 10,
-                                                                                               sort = "likeNumber",
-                                                                                               direction = Sort.Direction.DESC,
-                                                                                               page = 0) Pageable pageable){
+    public BaseResponse<Slice<ReviewProgramResponseDto>> showUserSpecificReviewListALL(
+        @PathVariable("programId") Long programId,
+        @PageableDefault(size = 10,
+            sort = "likeCounts",
+            direction = Sort.Direction.DESC,
+            page = 0) Pageable pageable) {
         User findUser = getUser();
-        return BaseResponse.success(reviewShowProgramDetailService.showUserSpecificReviewList(findUser,programId,pageable));
+        return BaseResponse.success(
+            reviewShowProgramDetailService.showUserSpecificReviewList(findUser, programId,
+                pageable));
     }
 
 
-
-
-
+    @ApiOperation(value = "자신의 취향에 맞는 사람들의 리뷰의 평균 별점 보여주기", notes = "자신의 취향에 맞는 사람들의 리뷰의 평균 별점 보여줍니다")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "programId", dataType = "long", value = "현재 프로그램의 ID", required = true, paramType = "path"),
+    })
     @GetMapping("/user/specific/review/rating")
     @ResponseStatus(HttpStatus.OK)
-    public BaseResponse<String> showUserSpecificReviewRating(@PathVariable("programId") Long programId){
-        User findUser= getUser();
-        return BaseResponse.success(reviewShowProgramDetailService.showUserSpecificRating(findUser,programId));
+    public BaseResponse<String> showUserSpecificReviewRating(
+        @PathVariable("programId") Long programId) {
+        User findUser = getUser();
+        return BaseResponse.success(
+            reviewShowProgramDetailService.showUserSpecificRating(findUser, programId));
     }
 
-    private User getUser(){
-        return userRepository.findByEmail(SecurityUtil.getCurrentEmail().get()).orElseThrow(()->new NotFoundException(ErrorCode.ENTITY_NOT_FOUND));
+    private User getUser() {
+        return userRepository.findByEmail(SecurityUtil.getCurrentEmail().get())
+            .orElseThrow(() -> new NotFoundException(ErrorCode.ENTITY_NOT_FOUND));
     }
 }
--- src/main/java/tavebalak/OTTify/review/controller/ReviewTagController.java
+++ src/main/java/tavebalak/OTTify/review/controller/ReviewTagController.java
@@ -1,5 +1,9 @@
 package tavebalak.OTTify.review.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,21 +14,25 @@ import tavebalak.OTTify.review.dto.reviewtagRequest.ReviewSaveTagDto;
 import tavebalak.OTTify.review.dto.reviewtagResponse.ReviewTagListResponseDto;
 import tavebalak.OTTify.review.service.ReviewTagService;
 
-import javax.validation.Valid;
-
 @RestController
 @RequiredArgsConstructor
+@Api(tags = {"리뷰 태그 컨트롤러"})
+
 public class ReviewTagController {
+
     private final ReviewTagService reviewTagService;
 
+    @ApiOperation(value = "리뷰 태그 저장", notes = "추후에 리뷰 태그를 따로 저장하는 로직이 필요할 때를 위해 남겨두었습니다")
+    @ApiResponse(code = 200, message = "리뷰 태그 저장 완료")
     @PostMapping("/api/v1/reviewTag")
-    public BaseResponse saveReviewTag(@RequestBody @Valid ReviewSaveTagDto reviewSaveTagDto){
+    public BaseResponse saveReviewTag(@RequestBody @Valid ReviewSaveTagDto reviewSaveTagDto) {
         reviewTagService.saveReviewTag(reviewSaveTagDto);
         return BaseResponse.success("리뷰 태그 저장 완료 ");
     }
 
+    @ApiOperation(value = "저장된 리뷰 태그들을 보여줍니다", notes = "리뷰 태그 리스트를 보여줄 때 사용합니다")
     @GetMapping("/api/v1/reviewTag/list")
-    public BaseResponse<ReviewTagListResponseDto> showReviewTags(){
+    public BaseResponse<ReviewTagListResponseDto> showReviewTags() {
         return BaseResponse.success(reviewTagService.showReviewTagList());
     }
 }
