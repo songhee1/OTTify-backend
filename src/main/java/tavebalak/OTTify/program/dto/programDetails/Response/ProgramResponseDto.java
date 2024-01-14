7778ac0c71fa6cfd2c4fde26f10201d33222b26e
Jinu Yi <dionisos198@naver.com>
2024-01-14 19:04:03 +0900

    Feat: 프로그램 상세 정보에 평균 별점 넣기 (#41)

--- src/main/java/tavebalak/OTTify/program/dto/programDetails/Response/ProgramResponseDto.java
+++ src/main/java/tavebalak/OTTify/program/dto/programDetails/Response/ProgramResponseDto.java
@@ -6,13 +6,18 @@ import tavebalak.OTTify.program.dto.programDetails.openApiRequest.providerDetail
 
 @Getter
 public class ProgramResponseDto {
+
     private ProgramDetailResponse programDetailResponse;
     private OAProgramCreditsDto oaProgramCreditsDto;
     private OACountryDetailsDto OACountryDetailsDto;
+    private String programNormalReviewRating;
 
-    public ProgramResponseDto(ProgramDetailResponse programDetailResponse, OAProgramCreditsDto oaProgramCreditsDto, OACountryDetailsDto OACountryDetailsDto){
+    public ProgramResponseDto(ProgramDetailResponse programDetailResponse,
+        OAProgramCreditsDto oaProgramCreditsDto, OACountryDetailsDto OACountryDetailsDto,
+        double programNormalReviewRating) {
         this.programDetailResponse = programDetailResponse;
-        this.oaProgramCreditsDto=oaProgramCreditsDto;
+        this.oaProgramCreditsDto = oaProgramCreditsDto;
         this.OACountryDetailsDto = OACountryDetailsDto;
+        this.programNormalReviewRating = String.format("%.1f", programNormalReviewRating);
     }
 }
--- src/main/java/tavebalak/OTTify/program/service/ProgramDetailsShowServiceImpl.java
+++ src/main/java/tavebalak/OTTify/program/service/ProgramDetailsShowServiceImpl.java
@@ -66,7 +66,7 @@ public class ProgramDetailsShowServiceImpl implements ProgramDetailsShowService 
 
         // 사용자에게 보내줄 DTO
         ProgramResponseDto programResponseDto = new ProgramResponseDto(programDetailResponse,
-            oaProgramCreditsDto, kr);
+            oaProgramCreditsDto, kr, program.getAverageRating());
         return programResponseDto;
 
     }
