2cdee9d7c4c540b1370a068c118c88b3b95a3cf1
Jinu Yi <dionisos198@naver.com>
2024-01-15 14:39:01 +0900

    Fix: localhost 3000도 추가 (#74)

--- src/main/java/tavebalak/OTTify/common/config/WebConfig.java
+++ src/main/java/tavebalak/OTTify/common/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
             .allowedOrigins("http://ottify.kro.kr:3000") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
+            .allowedOrigins("http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "PATCH"); // cors를 허용할 method
     }
 }
