2bccdf876f0dadfa9525b62a5bee545e084d8c90
Jinu Yi <dionisos198@naver.com>
2024-01-16 18:56:12 +0900

    Refactor: 감독과 배우만 보여주도록 수정 및 ott 관련 에러 수정 (#79)

--- src/main/java/tavebalak/OTTify/program/dto/programDetails/openApiRequest/personDetails/OAProgramCreditsDto.java
+++ src/main/java/tavebalak/OTTify/program/dto/programDetails/openApiRequest/personDetails/OAProgramCreditsDto.java
@@ -1,18 +1,20 @@
 package tavebalak.OTTify.program.dto.programDetails.openApiRequest.personDetails;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
-import lombok.Getter;
-
 import java.util.List;
+import lombok.Getter;
 
 @Getter
 public class OAProgramCreditsDto {
-    private Long id;
+
+    //private Long id;
     private List<Cast> cast;
     private List<Cast> crew;
 
-    public void changeCast(List<Cast> cast){
-        this.cast=cast;
+    public void changeCast(List<Cast> cast) {
+        this.cast = cast;
+    }
+
+    public void changeCrew(List<Cast> crew) {
+        this.crew = crew;
     }
 }
