ed8a5401afbf1be00884a1e7566d8b392ef4901a
Jinu Yi <dionisos198@naver.com>
2024-01-16 17:37:38 +0900

    Fix: allowed origin 및 메서드 수정 (#80)

--- src/main/java/tavebalak/OTTify/common/config/WebConfig.java
+++ src/main/java/tavebalak/OTTify/common/config/WebConfig.java
@@ -10,10 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // cors를 적용할 spring서버의 url 패턴.
-            .allowedOrigins("http://ottify.kro.kr:3000") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
-            .allowedOrigins("http://ottify.kro.kr")//새로 추가된 부분 : 여기
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://ottify.kro.kr:3000", "http://ottify.kro.kr",
+                "http://localhost:3000") // cors를 허용할 도메인. 제한을 모두 해제하려면 "**"
             .allowedMethods("GET", "POST", "PUT", "PATCH",
-                "DELETE"); // cors를 허용할 method + DELETE 추가
+                "DELETE", "OPTIONS"); // cors를 허용할 method + DELETE 추가
     }
 }
