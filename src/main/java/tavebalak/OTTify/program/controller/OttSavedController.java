55f9e9f98c5ab0fe7605d5f11de2572b7b647172
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-17 23:03:25 +0900

    Refactor: 컨트롤러 이름 변경 & 엔드포인트 수정 (#99)

--- src/main/java/tavebalak/OTTify/program/controller/OttSavedController.java
+++ src/main/java/tavebalak/OTTify/program/controller/OttSavedController.java
@@ -15,15 +15,15 @@ import tavebalak.OTTify.program.service.OttService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/ott")
+@RequestMapping("/api/v1/saved/ott")
 @Api(tags = {"OTT 컨트롤러"})
-public class OttController {
+public class OttSavedController {
 
     private final OttService ottService;
 
     @ApiOperation(value = "저장된 OTT 리스트 조회", notes = "저장된 모든 OTT의 아이디와 이름을 조회합니다.")
     @ApiResponse(code = 200, message = "성공적으로 저장된 OTT 리스트를 조회하였습니다.")
-    @GetMapping("/")
+    @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     public BaseResponse<OttListDTO> getOttList() {
         return BaseResponse.success(ottService.getOttList());
