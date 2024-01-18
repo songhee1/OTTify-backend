0a4ce8d019ea2176ea5bb9c836daf4aeb9d61459
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-18 19:51:10 +0900

    Feature: S3 upload 구현 (#107)

---
+++ src/main/java/tavebalak/OTTify/common/s3/AWSS3Service.java
@@ -0,0 +1,52 @@
+package tavebalak.OTTify.common.s3;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.util.IOUtils;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import tavebalak.OTTify.error.ErrorCode;
+import tavebalak.OTTify.error.exception.BadRequestException;
+import tavebalak.OTTify.error.exception.InternalServerErrorException;
+
+@Service
+@RequiredArgsConstructor
+public class AWSS3Service {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3Client amazonS3Client;
+
+    public String upload(MultipartFile multipartFile, String dirName) {
+        String fileName = dirName + "/" + UUID.randomUUID() + ".jpg";
+        if (multipartFile.isEmpty()) {
+            throw new BadRequestException(ErrorCode.PROFILE_PHOTO_ISNOT_EXIST);
+        }
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            objectMetadata.setContentLength(bytes.length);
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+
+            amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, byteArrayInputStream, objectMetadata)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+        } catch (IOException e) {
+            throw new InternalServerErrorException(ErrorCode.UPLOAD_FAILED);
+        }
+
+        return amazonS3Client.getUrl(bucket, fileName).toString();
+    }
+
+}
