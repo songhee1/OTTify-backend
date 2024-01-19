98e0e42c3f5cfa009a5dc7a191d84a2f54dc36b4
Jinu Yi <dionisos198@naver.com>
2024-01-19 17:43:14 +0900

    Refactor: CountByGenreName 으로 변경 (#111)

--- src/main/java/tavebalak/OTTify/review/repository/ReviewRepository.java
+++ src/main/java/tavebalak/OTTify/review/repository/ReviewRepository.java
@@ -37,7 +37,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
         @Param("genre") String genre, Pageable pageable);
 
     @Query("select count(r) from Review r where r.genre=:genre and r.program=:program")
-    int countByMyGenreName(@Param("genre") String genre, @Param("program") Program program);
+    int countByGenreName(@Param("genre") String genre, @Param("program") Program program);
 
     @Query("select sum(r.rating) from Review r where r.genre=:genre and r.program=:program")
     Double sumReviewRatingByGenreName(@Param("genre") String genreName,
