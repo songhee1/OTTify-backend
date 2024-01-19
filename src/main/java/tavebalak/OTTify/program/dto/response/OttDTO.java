ff663052469f1501a0124b7f9fc16f447bbc9739
jyjyjy25 <jyjyjy7418@gmail.com>
2024-01-20 01:56:16 +0900

    Refactor: OttDTO에 subscribeLogoPath 필드 추가 (#115)

--- src/main/java/tavebalak/OTTify/program/dto/response/OttDTO.java
+++ src/main/java/tavebalak/OTTify/program/dto/response/OttDTO.java
@@ -8,14 +8,19 @@ import tavebalak.OTTify.program.entity.Ott;
 @Getter
 @NoArgsConstructor
 public class OttDTO {
+
     @ApiModelProperty(value = "OTT id")
     private Long id;
 
     @ApiModelProperty(value = "OTT 이름")
     private String name;
 
+    @ApiModelProperty(value = "구독 중인 OTT 로고 url")
+    private String subscribeLogoPath;
+
     public OttDTO(Ott ott) {
         this.id = ott.getId();
         this.name = ott.getName();
+        this.subscribeLogoPath = ott.getSubscribeLogoPath();
     }
 }
